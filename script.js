dbe399d54f52f0c43471a26f612c58649aec65d3
Nanda Indoor <89999541+Nanda-123@users.noreply.github.com>
2022-05-27 07:41:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Nanda", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for(var i=0;i<(names.length);i++){
     var upper=names[i].charAt(0);
     var lower=upper.toLowerCase();
